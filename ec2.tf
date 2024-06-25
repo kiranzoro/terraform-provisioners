d69c171a318b11ec44671804fbd393e7210d97f4
kiranzoro <kirankumar333444@gmail.com>
2024-06-25 17:45:06 +0530

    proviisoners

--- ec2.tf
+++ ec2.tf
@@ -3,18 +3,26 @@ resource "aws_instance" "db"{
     vpc_security_group_ids = ["sg-0c4aa78fe1bbd2e9e"]
     instance_type = "t2.micro"
 
-    tags = {
-        Name = "DB"
-
-    }
-
 
   provisioner "local-exec" {
-    command = "echo  ${self.private_ip} > inventory.txt "
-
+    command = "echo  ${self.private_ip} > inventory.txt"
   }
-  provisioner "local-exec" {
-    command = "ansible-playbook -i inventory.txt -e ansible_user=ec2-user -e ansible_password=DevOps321 web.yaml" 
+  connection {
+  type     = "ssh"
+  user     = "ec2-user"
+  password = "DevOps321"
+  host     = self.public_ip
+  # }
+  # provisioner "local-exec" {
+  #   command = "ansible-playbook -i inventory.txt -e ansible_user=ec2-user -e ansible_password=DevOps321 web.yaml" 
 
+  # }
+
+}
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install ansible -y",
+      "sudo dnf install nginx -y"
+    ]
   }
 }
